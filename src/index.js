5e9097d1f381aa6a2ed9ad5a16c153ddacc7fd78
Meirbek <53935454+MeirbekAshirbayev@users.noreply.github.com>
2024-07-30 10:12:08 +0500

    feat:index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const str = "" + n;
+    const regex = /[-]+/;
+    let s = str.replace(regex, "");
+    console.log(s)
+    let number = s.split('').reverse().join('');
+    return +number
 }
